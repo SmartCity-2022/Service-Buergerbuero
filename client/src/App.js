02aa51038adece00c3f91f4240f44019479c4e34
Finn Bechinka <contact@bechinka.eu>
2022-05-04 13:40:10 +0200

    added lost property table
    for showing working backend - frontend communication

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,40 @@
 import "./App.css";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+    const [lost, set_lost] = useState([]);
     useEffect(() => {
-        axios.get("http://localhost:3001/test").then((res) => {
-            console.log(res.data);
+        axios.get("http://localhost:3001/lost_property").then((res) => {
+            console.log();
+            set_lost(res.data);
         });
     }, []);
 
-    return <div className="App">buegerbuero</div>;
+    return (
+        <div className="App">
+            <h1>Fundsachen</h1>
+            <table bgcolor="black" align="center">
+                <tbody>
+                    <tr bgcolor="grey">
+                        <th width="400">Datum gefunden</th>
+                        <th width="400">Typ</th>
+                        <th width="400">Beschreibung</th>
+                    </tr>
+
+                    {lost.map((value, key) => {
+                        return (
+                            <tr key={key} bgcolor="lightgrey">
+                                <td>{value.found_on}</td>
+                                <td>{value.type}</td>
+                                <td>{value.desc}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
