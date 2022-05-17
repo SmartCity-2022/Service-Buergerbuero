c6138713e39ea426c2759fe15aa247ed0d9b1598
Finn <contact@bechinka.eu>
2022-05-17 22:31:35 +0200

    added rabbitmq amqp host as env var

--- server/rabbitmq.js
+++ server/rabbitmq.js
@@ -1,11 +1,9 @@
 const amqp = require("amqplib");
+require("dotenv").config();
 
-const connection = amqp.connect(
-    "amqp://localhost:5672",
-    (error0, connecttion) => {
-        if (error0) throw error0;
-    }
-);
+const connection = amqp.connect(process.env.AMQPHOST, (error0, connecttion) => {
+    if (error0) throw error0;
+});
 
 function connect() {
     return connection;
