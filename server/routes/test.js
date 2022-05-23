ca70fdb42f2c43ae6da63f9e9ceb6522271cd6ae
Finn <contact@bechinka.eu>
2022-05-24 00:36:40 +0200

    added test endpoint to create mock data

--- server/routes/test.js
+++ server/routes/test.js
@@ -4,10 +4,16 @@ const db = require("../models");
 const rabbitmq = require("../rabbitmq");
 const { auth } = require("../middlewares/auth");
 require("dotenv").config();
+const { create_mockdata } = require("../mockdata");
 
 router.get("/", auth, async (req, res) => {
     console.log("test auth");
     res.send("OK");
+});
+
+router.get("/mock", async (req, res) => {
+    await create_mockdata(5);
+    res.send("mockdata created");
 });
 
 module.exports = router;
