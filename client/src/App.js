9e0b66c678feadce7750043b9ad586b798ed10eb
Finn <contact@bechinka.eu>
2022-05-24 16:05:53 +0200

    check if obj is array before mapping

--- client/src/App.js
+++ client/src/App.js
@@ -56,16 +56,21 @@ function App() {
                         <th width="400">Nachname</th>
                         <th width="400">E-Mail</th>
                     </tr>
-
-                    {citizen.map((value, key) => {
-                        return (
-                            <tr key={key} bgcolor="lightgrey">
-                                <td>{value.first_name}</td>
-                                <td>{value.last_name}</td>
-                                <td>{value.email}</td>
-                            </tr>
-                        );
-                    })}
+                    {Array.isArray(citizen) ? (
+                        citizen.map((value, key) => {
+                            return (
+                                <tr key={key} bgcolor="lightgrey">
+                                    <td>{value.first_name}</td>
+                                    <td>{value.last_name}</td>
+                                    <td>{value.email}</td>
+                                </tr>
+                            );
+                        })
+                    ) : (
+                        <>
+                            <h1>citizen ist kein array lol why</h1>
+                        </>
+                    )}
                 </tbody>
             </table>
             <button onClick={on_click}>test auth</button>
