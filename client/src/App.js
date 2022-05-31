9d1b7bdf73103c81ea99334bdf705760dc557d64
Finn <contact@bechinka.eu>
2022-05-31 23:37:17 +0200

    added page not found page

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,15 @@ import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 import Home from "./pages/home";
+import Page_Error from "./pages/page_error";
+
 function App() {
     return (
         <div className="App">
             <Router>
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="*" element={<Page_Error />} />
                 </Routes>
             </Router>
         </div>
