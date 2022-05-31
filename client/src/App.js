f3c71db935f4c700e6e6ffbd1d7bb05eb1fc8acd
Finn <contact@bechinka.eu>
2022-05-31 23:23:02 +0200

    pages folder structure; created home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,17 @@
 import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import Cookies from "js-cookie";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
+import Home from "./pages/home";
 function App() {
-    const [citizen, set_citizen] = useState([]);
-    useEffect(() => {
-        axios
-            .get(`${process.env.REACT_APP_BACKEND_HOST}/citizen`)
-            .then((res) => {
-                set_citizen(res.data);
-            });
-    }, []);
-
-    const on_click = async () => {
-        axios
-            .get(`${process.env.REACT_APP_BACKEND_HOST}/test`, {
-                withCredentials: true,
-            })
-            .then((res) => {
-                console.log(res.data);
-            })
-            .catch((obj) => {
-                console.log(obj.response.data);
-            });
-    };
-
-    const on_click_mock = async () => {
-        axios
-            .get(`${process.env.REACT_APP_BACKEND_HOST}/test/mock`)
-            .then((res) => {
-                console.log(res.data);
-                window.location.reload(false);
-            })
-            .catch((obj) => {
-                console.log(obj.response.data);
-            });
-    };
-
     return (
         <div className="App">
-            <h1>Bürger</h1>
-            <table bgcolor="black" align="center">
-                <tbody>
-                    <tr bgcolor="grey">
-                        <th width="400">Vorname</th>
-                        <th width="400">Nachname</th>
-                        <th width="400">E-Mail</th>
-                    </tr>
-                    {Array.isArray(citizen) &&
-                        citizen.map((value, key) => {
-                            return (
-                                <tr key={key} bgcolor="lightgrey">
-                                    <td>{value.first_name}</td>
-                                    <td>{value.last_name}</td>
-                                    <td>{value.email}</td>
-                                </tr>
-                            );
-                        })}
-                </tbody>
-            </table>
-            <button onClick={on_click}>test auth</button>
-            <button onClick={on_click_mock}>add mock data</button>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Router>
         </div>
     );
 }
---
+++ client/src/pages/home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <h1>Willkommen zum Bürgerbüro</h1>
+        </div>
+    );
+}
+
+export default Home;
