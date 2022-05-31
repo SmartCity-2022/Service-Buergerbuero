8db4eafd92bb95173f3388b9bb63ca1114c07e1f
Finn <contact@bechinka.eu>
2022-06-01 00:42:01 +0200

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,18 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 import Home from "./pages/home";
 import Page_Error from "./pages/page_error";
+import Nav from "./components/navbar";
 
 function App() {
     return (
         <div className="App">
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="*" element={<Page_Error />} />
-                </Routes>
-            </Router>
+            <Nav />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="home" element={<Home />} />
+                <Route path="error" element={<Page_Error />} />
+                <Route path="*" element={<Page_Error />} />
+            </Routes>
         </div>
     );
 }
