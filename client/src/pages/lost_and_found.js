63e5deb364e2927f972738e27488fbca38817a65
Finn <contact@bechinka.eu>
2022-06-03 15:36:47 +0200

    duden

--- client/src/pages/lost_and_found.js
+++ client/src/pages/lost_and_found.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Box, Typography, Divider } from "@mui/material";
@@ -31,29 +31,33 @@ function Lost_and_found() {
 
     return (
         <>
-            <Box sx={{ width: "100%", my: 5 }}>
-                <Typography variant="h2" align="center" gutterBottom>
-                    Fundsachen
-                </Typography>
-                <Typography sx={{ mx: "5%" }} variant="h6" align="center">
-                    Hier finden Sie eine Liste aller Fundsachen, welche bei uns
-                    hinterlegt sind.
-                </Typography>
-                <Typography
-                    sx={{ mx: "5%" }}
-                    variant="h6"
-                    align="center"
-                    gutterBottom
-                >
-                    Wenn Sie in letzter Zeit etwas verloren haben und glauben,
-                    dass einer dieser Gegenstände Ihnen gehören könnte, dann
-                    melden Sie sich bitte bei uns.
-                </Typography>
-                <Divider />
-                <Box sx={{ height: 600, width: "80%", m: "auto", my: "1%" }}>
-                    <DataGrid rows={rows} columns={columns} pageSize={9} />
+            <Suspense>
+                <Box sx={{ width: "100%", my: 5 }}>
+                    <Typography variant="h2" align="center" gutterBottom>
+                        Fundsachen
+                    </Typography>
+                    <Typography sx={{ mx: "5%" }} variant="h6" align="center">
+                        Hier finden Sie eine Liste aller Fundsachen, welche bei
+                        uns hinterlegt sind.
+                    </Typography>
+                    <Typography
+                        sx={{ mx: "5%" }}
+                        variant="h6"
+                        align="center"
+                        gutterBottom
+                    >
+                        Wenn Sie in letzter Zeit etwas verloren haben und
+                        glauben, dass einer dieser Gegenstände Ihnen gehören
+                        könnte, dann melden Sie sich bitte bei uns.
+                    </Typography>
+                    <Divider />
+                    <Box
+                        sx={{ height: 600, width: "80%", m: "auto", my: "1%" }}
+                    >
+                        <DataGrid rows={rows} columns={columns} pageSize={9} />
+                    </Box>
                 </Box>
-            </Box>
+            </Suspense>
         </>
     );
 }
