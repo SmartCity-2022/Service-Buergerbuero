584e073d7971df940dd7d4d435c06842555d7fc8
Finn <contact@bechinka.eu>
2022-06-07 16:40:49 +0200

    reading response fix
    now reading response.data not response.body

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -40,7 +40,7 @@ const auth = async (req, res, next) => {
                 token: refresh_token,
             })
             .then((response) => {
-                const { accessToken } = response.body;
+                const { accessToken } = response.data;
                 if (accessToken) {
                     res.cookie("accessToken", accessToken, {
                         domain: ".smartcity.w-mi.de",
@@ -51,7 +51,7 @@ const auth = async (req, res, next) => {
                         return next();
                     }
                 } else {
-                    if (response.body.errMsg) {
+                    if (response.data.errMsg) {
                         console.log(`token refresh error:\n ${errMsg}`);
                     }
                     return res
