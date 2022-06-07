8da16be6670b6190437481238d905aaa51d5b4ed
Finn <contact@bechinka.eu>
2022-06-07 23:23:34 +0200

    init testing with jest

---
+++ server/tests/lost_property.test.js
@@ -0,0 +1,9 @@
+describe("lost_property", () => {
+    describe("get", () => {
+        describe("given no type parameter is set", () => {
+            it("should return the whole table", () => {
+                expect(true).toBe(true);
+            });
+        });
+    });
+});
